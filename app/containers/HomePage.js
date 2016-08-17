ab5a4ff614247728ecb21320beade72087c37f9d
Chris Vandenberg <chrisvan0848@gmail.com>
2016-08-17 16:39:52 -0700

    remove rdx

--- app/containers/HomePage.js
+++ app/containers/HomePage.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import Home from '../components/Home';
+import Home from '../components/Home/index';
 
 export default class HomePage extends Component {
   render() {
     return (
-      <Home />
+      <div>
+        <Home />
+      </div>
     );
   }
 }
